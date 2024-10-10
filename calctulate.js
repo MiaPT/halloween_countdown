82991ef18f5890d51615ac19c7cd459e03dac7fb
Mia Pernille Treland <mia.treland@gmail.com>
2024-10-10 22:09:36 +0200

    countdown + spooky custom text

---
+++ calctulate.js
@@ -0,0 +1,25 @@
+import dayjs from 'dayjs'
+
+const today = dayjs()
+
+const afterHalloween = today.month() > 10
+
+const halloween = dayjs(new Date(today.year() + afterHalloween, 9, 31))
+
+
+let daysLeft = halloween.diff(today, "day")
+
+let timeLeft = daysLeft
+let unit = "DAGER"
+
+if (timeLeft < 3){
+    timeLeft =  halloween.diff(today, "hour")
+    unit = "TIMER"
+}
+
+
+document.getElementById("countdown").innerHTML = "" + timeLeft + " " + unit
+
+
+
+
