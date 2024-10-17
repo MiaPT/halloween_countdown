0125632229b96d1e65c233708020befea6868f69
Mia Pernille Treland <mia.treland@gmail.com>
2024-10-17 21:03:15 +0200

    move code for spinning emojis into main.js

--- calctulate.js
+++ calctulate.js
@@ -19,23 +19,3 @@ if (timeLeft < 3){
 
 
 document.getElementById("countdown").innerHTML = "" + timeLeft + " " + unit
-
-
-const emojiList = ["🎃",  "👽", "🕷️", "💀", "🦇", "🕸️", "🍬"]
-const animationDuration = 24
-
-emojiList.forEach((emoji, index) => {
-    const emojiElement = document.createElement("div")
-    
-    const css = { 
-        position: "absolute",
-        animationName: "spin",
-        animationDuration: `${animationDuration}s`,
-        animationIterationCount: "infinite",
-        animationTimingFunction: "linear",
-        animationDelay: `-${(animationDuration/emojiList.length) * index}s`,
-     }
-     Object.assign(emojiElement.style, css)
-    emojiElement.innerHTML = emoji
-    document.getElementById("emojis").appendChild(emojiElement)
-})
