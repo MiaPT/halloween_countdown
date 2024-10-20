3d6b11f2450f162513722829e6d33129943ff674
Mia Pernille Treland <mia.treland@gmail.com>
2024-10-20 20:38:59 +0200

    correct number of days left (increase by 1)

--- calctulate.js
+++ calctulate.js
@@ -7,9 +7,9 @@ const afterHalloween = today.month() > 10
 const halloween = dayjs(new Date(today.year() + afterHalloween, 9, 31))
 
 
-let daysLeft = halloween.diff(today, "day")
-
-let timeLeft = daysLeft
+// +1 because the diff between one day and the next is 0. 
+// This will be wrong between 00:00:00 and 00:00:01, so don't do that when that matters:)
+let timeLeft = halloween.diff(today, "day") + 1
 let unit = "DAGER"
 
 if (timeLeft < 3){
